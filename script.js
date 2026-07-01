07c3558783cb27736f3d47b6bc8b984ef78527a8
Abhay Chaturvedi <109450662+abhay707@users.noreply.github.com>
2026-07-01 18:02:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,22 +3,7 @@ function indexOfIgnoreCase(s1, s2) {
 	str1 = s1.toLowerCase();
 	str2 = s2.toLowerCase();
 
-	let m = str1.length;
-	let n = str2.length;
-
-	for(let i = 0;i<= m-n;i++){
-		let j;
-		for(j = 0;j<m;j++){
-			if(str1[i+j] !== str2[j]){
-				break;
-			}
-		}
-		if(j === m){
-			return i;
-		}
-		
-	}
-	return -1;
+	return str1.indexOf(str2);
 }
 
 // Please do not change the code below
